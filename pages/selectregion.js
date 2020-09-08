f9375794df8dea42acbebfc418f890e658d7e022
claratheodora <theodoraclara@gmail.com>
2020-09-08 23:04:57 +0700

    select category

--- pages/selectregion.js
+++ pages/selectregion.js
@@ -3,8 +3,12 @@ const { By, Key} = require("selenium-webdriver");
 async function selectRegion(driver){
     await driver.findElement(By.linkText('Pembayaran')).click();
 
-    await driver.findElement(By.id('cabang')).click();
-    await driver.findElement(By.css('#cabang > option:nth-child(1)')).click();
+    await driver.findElement(By.xpath("//input[@value='- Pilih Cabang / Kategori -']")).click();
+    await driver.findElement(By.xpath("//li[@class=''][1]")).click();
+    
+    //await driver.findElement(By.css("input:contains('Surabaya')")).click();
+    //await driver.findElement(By.xpath("//input[@class='.select-dropdown'][2]")).click();
+    //await driver.findElement(By.xpath("//li[@class='']")).click();
 
 }
 module.exports = selectRegion;
